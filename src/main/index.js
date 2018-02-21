1a0ea6d95d96b6efc8c1613710585133b2323425
Oleksandr Dziuban <oleksandr_dziuban@epam.com>
2018-02-21 12:15:20 +0200

    Rendering fixed

--- src/main/index.js
+++ src/main/index.js
@@ -10,9 +10,9 @@ let mainWindow;
  * @returns {Electron.BrowserWindow}
  */
 const createMainWindow = () => {
-  const window = new BrowserWindow();
-
-  window.maximize();
+  const window = new BrowserWindow({
+    show: false,
+  });
 
   if (isDevelopment) {
     window.webContents.openDevTools();
@@ -28,6 +28,11 @@ const createMainWindow = () => {
       slashes: true
     }));
   }
+
+  window.once('ready-to-show', () => {
+    window.maximize();
+    window.show();
+  });
 
   window.on('closed', () => {
     mainWindow = null
